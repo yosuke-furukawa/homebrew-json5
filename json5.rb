a4c0c2feac81f4d4f14172f522fa2bd68cddd392
yosuke-furukawa <yosuke.furukawa@gmail.com>
2014-12-10 02:06:21 +0900

    Bump v0.1.1

--- json5.rb
+++ json5.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class Json5 < Formula
-  homepage 'https://github.com/yosuke-furukawa/gojson5/'
+  homepage 'https://github.com/yosuke-furukawa/json5/'
 
   if Hardware.is_64_bit?
-    url "https://github.com/yosuke-furukawa/gojson5/raw/master/pkg/dist/json5_0.1.0_darwin_amd64.zip"
-    sha1 "d5c35d87c5f539070fa022ae6aa9ed239358842f" 
+    url "https://github.com/yosuke-furukawa/json5/releases/download/v0.1.1/json5_0.1.1_darwin_amd64.zip"
+    sha1 "3698b7b095bd2b900f174ea3df26a8ecae449b00" 
   else
-    url "https://github.com/yosuke-furukawa/gojson5/raw/master/pkg/dist/json5_0.1.0_darwin_386.zip"
-    sha1 "d344e3ed785295e57a97fcfb8b8be9d14c668498"
+    url "https://github.com/yosuke-furukawa/json5/releases/download/v0.1.1/json5_0.1.1_darwin_386.zip"
+    sha1 "3086f536137e209074c3752f5fcf37012e3eb6db"
   end
 
   def install
